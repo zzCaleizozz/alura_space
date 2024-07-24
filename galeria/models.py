ec476de46aefef948e966c7841ee2be6314adffd
alessandrovieira <alessandroleal609@gmail.com>
2024-07-24 17:31:25 -0300

    .

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+
+    opcoes_categoria = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=200, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
+
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
+
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
