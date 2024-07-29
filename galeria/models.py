0a9c9170180e97030f9c3e8d84aeaa233dd4870e
alessandrovieira <alessandroleal609@gmail.com>
2024-07-29 14:27:26 -0300

    .

--- galeria/models.py
+++ galeria/models.py
@@ -1,19 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
 
     opcoes_categoria = [
-        ('NEBULOSA', 'Nebulosa'),
-        ('ESTRELA', 'Estrela'),
-        ('GALÁXIA', 'galáxia'),
-        ('PLANETA', 'Planeta'),
+        ('Nebulosa', 'Nebulosa'),
+        ('Estrela', 'Estrela'),
+        ('Galáxia', 'galáxia'),
+        ('Planeta', 'Planeta'),
     ]
 
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=200, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=opcoes_categoria, default='')
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
