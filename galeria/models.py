bd75874f50bfcf1b14a849d14720978ca3690c93
alessandrovieira <alessandroleal609@gmail.com>
2024-07-30 16:48:42 -0300

    .

--- galeria/models.py
+++ galeria/models.py
@@ -15,9 +15,9 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=opcoes_categoria, default='')
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
 
