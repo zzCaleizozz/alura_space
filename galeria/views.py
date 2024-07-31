ba01572c18ffd718de5c6f9d236cfc8f859c9568
alessandrovieira <alessandroleal609@gmail.com>
2024-07-31 14:34:17 -0300

    .

--- galeria/views.py
+++ galeria/views.py
@@ -10,3 +10,13 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+
+def buscar(request,):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    if 'buscar' in request.GET:
+        nome_buscar = request.GET['buscar']
+        if nome_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_buscar)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
